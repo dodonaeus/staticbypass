a9996c10bb88208cb60da3b0b0c5d38427078d85
Bill Chenyoung <bchenyoung@gmail.com>
2026-07-20 14:44:53 +1000

    Extended brainfuck implementation

--- c/obfuscators/Brainfuck.py
+++ c/obfuscators/Brainfuck.py
@@ -14,25 +14,64 @@ class Brainfuck:
 unsigned char * {name}(const unsigned char *encoded)
 {{
     unsigned char *out = calloc({size}, sizeof(unsigned char));
-    int j = 0;
-    for (int i =0; i < strlen(encoded); i++){{
-
-        switch(encoded[i]){{
-            case '>': j++; break;
-            case '+': out[j]++; break;
+    unsigned char *stack = calloc(10, sizeof(unsigned char));
+    int stackPointer = 0;
+    int outIndex = 0;
+    int instructionPointer = 0;
+    while (instructionPointer < {len} - 1){{
+        switch(encoded[instructionPointer]){{
+            case '>': 
+                stackPointer++; 
+                break;
+            case '+': 
+                stack[stackPointer]++; 
+                break;
+            case '-':
+                stack[stackPointer]--;
+                break;
+            case '.':
+                out[outIndex] = stack[stackPointer];
+                outIndex++;
+                break;
+            case '[':
+                if (stack[stackPointer] == 0){{
+                    while (encoded[instructionPointer] != ']'){{
+                        instructionPointer++;
+                    }}
+                }}
+                break;
+            case ']':
+                if (stack[stackPointer] != 0){{
+                    while (encoded[instructionPointer] != '['){{
+                        instructionPointer--;
+                    }}
+                }}
+                break;
         }}
+        instructionPointer++;
+    }}
+
+    for (int i =0; i<{size}; i++){{
+        printf("%llx\\n", out[i]);
     }}
 
     return out;
 }}
-""".format(name = self.name, size = self.size)
+""".format(name = self.name, size = self.size, len=self.len)
     
     def compilerOptions(self):
         return []
 
     def obfuscate(self, decoded):
         self.size = len(decoded)
-        encoded = '>'.join(['+'*decoded[i] for i in range(0, len(decoded))])
+        encoded = '+' * decoded[0] + '.'
+        for i in range(1, len(decoded) - 1):
+            delta = decoded[i] - decoded[i - 1]
+            if delta < 0:
+                encoded += '-'*abs(delta) + '.'
+            else:
+                encoded += '+'*delta + '.'
+        self.len = len(encoded)
         return encoded
 
     def transformer(self, shellcodestring):
