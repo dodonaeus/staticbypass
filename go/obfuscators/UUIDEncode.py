cf1672d5d5fb8035fca2a87cd879d3908fdc0b2d
Bill Chenyoung <bchenyoung@gmail.com>
2026-08-21 13:43:42 +1000

    Added go UUIDEncode

--- go/obfuscators/UUIDEncode.py
+++ go/obfuscators/UUIDEncode.py
@@ -1,26 +1,27 @@
 import random
 import string
+from uuid import UUID
 
-class IPv6Obfuscate:
+class UUIDEncode:
 
     def __init__(self, arguments: dict) -> None:
         self.name = ''.join(random.SystemRandom().choice(string.ascii_uppercase) for _ in range(16))
 
     def imports(self) -> list[str]:
-        return ['"strings"',
-                '"strconv"']
+        return ['"github.com/google/uuid"']
 
     def compilerOptions(self) -> list[str]:
-        return []
+        return ['github.com/google/uuid']
 
     def obfuscate(self, decoded: bytes) -> list[str]:
         encoded = []
         self.size = 0
+        self.shellcodeSize = len(decoded)
         for i in range(0, len(decoded), 16):
             chunk = decoded[i:i+16]
             if len(chunk) < 16:
                 chunk = chunk + (b"\x90" * (16 - len(chunk)))
-            encoded.append(':'.join([ f'{chunk[n]:02x}{chunk[n+1]:02x}' for n in range(0, 16, 2)]))
+            encoded.append(str(UUID(bytes = chunk)))
             self.size += 1
         return encoded
 
@@ -29,15 +30,11 @@ class IPv6Obfuscate:
 
     def codeblock(self) -> str:
         return f"""
-func {self.name}(encoded []string) []byte {{
+func {self.name}(uuids []string) []byte {{
     decoded := make([]byte, {self.size} * 16)
-    for i, address := range encoded {{
-        hextets := strings.Split(address, ":")
-        for j, hextet := range hextets {{
-            converted, _ := strconv.ParseInt(hextet, 16, 64)
-            decoded[i*16+j*2] = byte(converted >> 8 & 255);
-            decoded[i*16+j*2+1] = byte(converted & 255);
-        }}
+    for i, uuidstring := range uuids {{
+        parsed, _ := uuid.Parse(uuidstring)
+        copy(decoded[i*16:(i+1)*16],parsed[:])
     }}
     return decoded
 }}
